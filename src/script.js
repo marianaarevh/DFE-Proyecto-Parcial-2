95c385126ab183c904721fb4015b3d9079656ae9
marianaarevh <marianaarevalo933@gmail.com>
2023-10-26 20:59:34 -0700

    PDFE 2: creacion de modal con estilos aplicados + modelo Task + cambio de fuente + base fetch-api

---
+++ src/script.js
@@ -0,0 +1,14 @@
+
+// Modelo
+
+class Task {
+    constructor(id, title, description, completed, priority, tag, dueDate) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.completed = completed;
+      this.priority = priority;
+      this.tag = tag;
+      this.dueDate = dueDate;
+    }
+}
