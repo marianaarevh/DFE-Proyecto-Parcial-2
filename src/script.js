7f1c8043ac3e2a83477e403797b7e5ad6fa702ef
marianaarevh <marianaarevalo933@gmail.com>
2023-10-26 23:11:46 -0700

    PDFE 2: cards para cada tarea + funcion mostrar tareas + intento fallido de formatear fecha

--- src/script.js
+++ src/script.js
@@ -12,3 +12,42 @@ class Task {
       this.dueDate = dueDate;
     }
 }
+
+function mapAPIToTask(data) {
+    return data.map(item => {
+        return new Task(
+            item.id,
+            item.title,
+            item.description,
+            item.completed,
+            item.priority,
+            item.tag,
+            new Date(item.dueDate)
+        );
+    });  
+}
+const task1= new Task(1,"Limpiar carro","Vaciar carro y llevarlo a lavar","no-completada","baja","Salud",Date("2023-10-28"));
+const task2= new Task(2,"Terminar trabajos","hacer todas las tareas, exposiciones y proyectos","no-completada","baja","Escuela",Date("2023-10-28"));
+const taskList = [task1,task2];
+
+/* views */
+function displayTasks(tasks) {
+    const list = document.getElementById('to-do-list');
+
+    tasks.forEach(task => {
+        const card = document.createElement('div');
+        card.className = 'list-element';
+        card.innerHTML = `
+                    <div class="task-information">
+                        <h3>${task.title} - Oct 26 2023 </h3>
+                        <p>description: ${task.description}</p>
+                        <p>status: ${task.completed}</p>
+                        <p>tag: ${task.tag}</p>
+                    </div>
+        `;
+        list.appendChild(card);
+    })
+}
+
+// controlador
+displayTasks(taskList);
